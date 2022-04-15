2d908cd6083bce2ea68601c68a01bceb8ea13264
DevShubam <65375709+DevShubam@users.noreply.github.com>
2022-04-15 10:00:40 -0400

    Create allowlist1.py

---
+++ scripts/allowlist1.py
@@ -0,0 +1,13 @@
+f = open("block.txt", errors="ignore")
+list1 = [i.strip() for i in f.readlines()]
+f.close()
+f = open("allow.txt", errors="ignore")
+list2 = [i.strip() for i in f.readlines()]
+f.close()
+def Diff(list1, list2):
+    return list(set(list1) - set(list2)) + list(set(list2) - set(list1))
+list1 = Diff(list1, list2)
+f = open("block.txt","w")
+for i in list1:
+    f.write(f"{i}\n")
+f.close()
