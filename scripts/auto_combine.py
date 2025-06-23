71caabb7fddaa1b704f96331c6bb4a37daa1b1bc
DevShubam <65375709+DevShubam@users.noreply.github.com>
2025-06-23 10:16:55 -0400

    Update auto_combine.py

--- scripts/auto_combine.py
+++ scripts/auto_combine.py
@@ -87,7 +87,12 @@ filter_sets = {
             'https://airvpn.org/api/dns_lists/?code=pornaway_sites&style=domains',
             'https://blocklistproject.github.io/Lists/adguard/porn-ags.txt',
             'https://raw.githubusercontent.com/ShadowWhisperer/BlockLists/master/RAW/Adult',
+            'https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/pornblock1', 
+            'https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/pornblock2', 
+            'https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/pornblock3',
             'https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/pornblock4',
+            'https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/pornblock5',
+            'https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/pornblock6',
             'https://nsfw.oisd.nl'
         ],
         'output_file': 'nsfw/nsfw_combined.txt',
